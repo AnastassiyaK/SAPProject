84b8b7369450851b2b26a0d3fdc9f28adcacfeb5
Nastassia Karatkova <Nastassia_Karatkova@epam.com>
2019-09-09 16:11:11 +0300

    Added a new layer Business.Services

---
+++ SAPBusiness.Services/API Services/User/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Configuration;
+using Core.REST_API.Cookies;
+using OpenQA.Selenium;
+using SAPBusiness.Services.Interfaces.API_UserService;
+using SAPBusiness.UserData.DeveloperCenter;
+using System.Collections.ObjectModel;
+
+namespace SAPBusiness.Services.API_Services.User
+{
+    public class UserService
+    {
+        protected IUserService _userService;
+        public UserService()
+        {
+
+            if (AppConfiguration.AppSetting["APIUserService:UserService"] == "RestSharp")
+            {
+                _userService = new RestSharpUserService();
+            }
+            if (AppConfiguration.AppSetting["APIUserService:UserService"] == ".NetStandard")
+            {
+                _userService = new StandardUserService();
+            }
+        }
+        public UserStatistics GetUserStatistics(ReadOnlyCollection<Cookie> cookies)
+        {
+            var _cookies = new CookiesService().ExtractCookies(cookies);
+            return _userService.GetStatistics(_cookies);
+        }
+    }
+}
---
+++ SAPBusiness.Services/Interfaces/API UserService/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using SAPBusiness.UserData.DeveloperCenter;
+using System.Net;
+
+namespace SAPBusiness.Services.Interfaces.API_UserService
+{
+    public interface IUserService
+    {
+        UserStatistics GetStatistics(CookieContainer cookies);
+    }
+}
