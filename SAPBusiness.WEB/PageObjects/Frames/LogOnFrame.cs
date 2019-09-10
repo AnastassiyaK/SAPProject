df69e08f69a42b35bd9120720e62c5141b0abf4e
Nastassia Karatkova <Nastassia_Karatkova@epam.com>
2019-09-10 17:41:08 +0300

    LogOnFrame was added.

---
+++ SAPBusiness.WEB/PageObjects/Frames/LogOnFrame.cs
@@ -0,0 +1,36 @@
+﻿using SAPBusiness.WEB.PageObjects.LogOn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAPBusiness.WEB.PageObjects.Frames
+{
+    public class LogOnFrame : BasePageObject<LogOnFrame>, ILogOnStrategy
+    {
+        private readonly LogOnSection logOnSection;
+
+        public LogOnFrame(BaseWebDriver driver) : base(driver)
+        {
+            logOnSection = new LogOnSection(_driver);
+        }
+
+        public void LogOn(User user)
+        {
+            _driver.WaitForElement(By.Id("IDS_UI_Window"), 15);
+
+            _driver.SwitchToFrame(_driver.FindElement(By.Id("IDS_UI_Window")));
+
+            logOnSection.UserNameInput.SendKeys(user.Login);
+            logOnSection.PasswordInput.SendKeys(user.Password);
+            logOnSection.LogOnButton.Click();
+
+            _driver.SwitchToDefaultContent();
+
+        }
+
+        protected override LogOnFrame WaitForLoad()
+        {
+            return this;
+        }
+    }
+}
