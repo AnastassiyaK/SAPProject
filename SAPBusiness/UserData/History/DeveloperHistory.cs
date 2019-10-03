6eaa996ef68bbd57b26ba5f6eeefa7bf0f75499a
Nastassia Karatkova <Nastassia_Karatkova@epam.com>
2019-10-03 16:40:46 +0300

    Change UserHistory for saving data both from JSON and CSV

---
+++ SAPBusiness/UserData/History/DeveloperHistory.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace SAPBusiness.UserData.History
+{
+    public class DeveloperHistory
+    {
+        [JsonProperty("completionDate")]
+        [CsvHelper.Configuration.Attributes.Name("Date Time")]
+        public string CompletionDate { get; set; }
+
+        [JsonProperty("completionTime")]        
+        [CsvHelper.Configuration.Attributes.Name("Time spent")]
+        public string CompletionTime { get; set; }
+
+        [JsonProperty("taskTitle")]
+        [CsvHelper.Configuration.Attributes.Name("Task Title")]
+        public string TaskTitle { get; set; }
+
+        [JsonProperty("taskType")]
+        [CsvHelper.Configuration.Attributes.Name("Task type")]
+        public string TaskType { get; set; }
+
+        [JsonProperty("taskUrl")]
+        [CsvHelper.Configuration.Attributes.Name("Task Url")]
+        public string TaskUrl { get; set; }
+    }
+}
---
+++ SAPBusiness/UserData/History/UserHistory.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SAPBusiness.UserData.History
+{
+    public class UserHistory
+    {
+        [JsonProperty("developerHistory")]
+        public IList<DeveloperHistory> History { get; set; }
+    }
+}
